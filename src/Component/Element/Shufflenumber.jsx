0078cf97e58e1a34c0e06403a0f1bf5c4a28ed9c
Wasay-Moshin <114741456+Wasay-Moshin@users.noreply.github.com>
2022-12-07 17:59:38 +0500

    shufflecard

--- src/Component/Element/Shufflenumber.jsx
+++ src/Component/Element/Shufflenumber.jsx
@@ -1,55 +1,57 @@
 import React, { useState } from "react";
+import { Image } from "react-bootstrap";
+import carddata from "../Layout/Carddata.json"
 
 function Shufflenumber() {
   const [data, setData] = useState("");
-  const number = [1, 2, 3, 4, 5];
+  const [image, setImage] =useState("");
+  const [id, setId] =useState()
+
   const randomnumber = () => {
-    const random_data = number[Math.floor(Math.random() * number.length)];
-    setData(random_data);
+    const random_data = carddata[Math.floor(Math.random() * carddata.length)];
+    console.log(random_data.tile);
+    setData(random_data.tile);
+    setImage(random_data.img);
+    setId(random_data.id)
   };
+
   return (
     <div className="shuffle">
       <div className=" container text-center">
         <div className="row">
-            <div className="col-md-1"></div>
-          <div className="col-md-2">
-            <div className="card">
-              <h1>Card 1</h1>
+          {carddata.map((items) => (
+            <div className="col-md-2">
+              {" "}
+              <div className="card" style={{ width: "10rem" }}>
+                {" "}
+                <img src={items.img} className="card-img-top img-fluid" alt="..." />{" "}
+                <div className="card-body">
+                  {" "}
+                  <p className="card-text">card {items.id}</p>{" "}
+                </div>{" "}
+              </div>{" "}
             </div>
-          </div>
-          <div className="col-md-2">
-            <div className="card">
-              <h1>Card 2</h1>
-            </div>
-          </div>{" "}
-          <div className="col-md-2">
-            <div className="card">
-              <h1>Card 3</h1>
-            </div>
-          </div>{" "}
-          <div className="col-md-2">
-            <div className="card">
-              <h1>Card 4</h1>
-            </div>
-          </div>{" "}
-          <div className="col-md-2">
-            <div className="card">
-              <h1>Card 5</h1>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="mt-4">
           <button className="btn btn-dark " onClick={randomnumber}>
             Select Number
           </button>
         </div>
-        <div className="row">
-            <div className="col-md-1"></div>
-          <div className="col-md-3 mt-4">
-            <div className="card mt-4">
-              <h1>Card {data}</h1>
+        <div className="row mt-5 d-flex justify-content-center">
+        <div className="col-md-4">
+              {" "}
+              <div className="card">
+                {" "}
+                <div className=" d-flex justify-content-center">
+                <img src={image} className=" img-fluid random-img mt-3"/>{" "}
+                </div>
+                <div className="card-body">
+                  {" "}
+                  <h5 className="card-title">{data}</h5>{" "}
+                </div>{" "}
+              </div>{" "}
             </div>
-          </div>
         </div>
       </div>
     </div>
